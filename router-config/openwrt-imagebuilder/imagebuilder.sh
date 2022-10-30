352d3f56a612948c4f8f68faaefeb02fd1901418
Falcordog <45631069+Falcordog@users.noreply.github.com>
2022-10-30 13:22:58 +1100

    Update imagebuilder.sh

--- router-config/openwrt-imagebuilder/imagebuilder.sh
+++ router-config/openwrt-imagebuilder/imagebuilder.sh
@@ -173,7 +173,7 @@ rebuild_firmware() {
         xz-utils ziptool zoneinfo-australia-nz zoneinfo-core zstd  \
         \
         luci luci-base luci-compat luci-i18n-base-en luci-i18n-base-zh-cn luci-lib-base  \
-        luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-proto-batman-adv  \
+        luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio  \
         luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
         luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
         luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay  \
